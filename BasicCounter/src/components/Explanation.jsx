15a4f31840548d92c504e8d67bece9428df605dc
Espadv69 <aitorjob69@gmail.com>
2025-03-10 19:33:12 +0100

    Add content to Explanation component with usage instructions

--- BasicCounter/src/components/Explanation.jsx
+++ BasicCounter/src/components/Explanation.jsx
@@ -1,5 +1,25 @@
 import './Explanation.css'
 
-const Explanation = () => {}
+const Explanation = () => {
+  return (
+    <div className="explanation">
+      <h2>How to use this counter</h2>
+
+      <ol className="ol__explanation">
+        <li className="li__explanation">
+          <p>
+            <strong>1.</strong> Create a state variable to handle the counter
+            value.
+          </p>
+          <img
+            src="/images/initHook.png"
+            alt="code that shows how to create a hook to handle counter value"
+            className="img__explanation"
+          />
+        </li>
+      </ol>
+    </div>
+  )
+}
 
 export default Explanation
