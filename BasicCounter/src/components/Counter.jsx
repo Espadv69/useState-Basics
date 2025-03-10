b30c4bdf6d0273ec793bd022c11274dba57a395f
Espadv69 <aitorjob69@gmail.com>
2025-03-10 18:54:29 +0100

    Add increment function to Counter component for state management

--- BasicCounter/src/components/Counter.jsx
+++ BasicCounter/src/components/Counter.jsx
@@ -2,7 +2,14 @@ import { useState } from 'react'
 import './Counter.css'
 
 const Counter = () => {
+  // Initialize the count state
   const [count, setCount] = useState(0)
+
+  // Function to increment the count
+  // This function will be called when the button is clicked
+  const increment = () => {
+    setCount(count + 1)
+  }
 }
 
 export default Counter
