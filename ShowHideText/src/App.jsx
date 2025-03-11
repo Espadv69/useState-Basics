233d802ac62a1acdd4c6a7ca10ac1de9cac3eb41
Espadv69 <aitorjob69@gmail.com>
2025-03-11 19:29:30 +0100

    Add Explanation component to App for enhanced user guidance

--- ShowHideText/src/App.jsx
+++ ShowHideText/src/App.jsx
@@ -1,9 +1,11 @@
 import ShowText from './components/ShowText/ShowText'
+import Explanation from './components/Explanation/Explanation'
 
 const App = () => {
   return (
     <div className="app">
       <ShowText />
+      <Explanation />
     </div>
   )
 }
