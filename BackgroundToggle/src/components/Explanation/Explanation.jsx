04f0fe1282d59c8af9b1a8ffbf06788aefaa1e6b
Espadv69 <aitorjob69@gmail.com>
2025-03-11 17:43:24 +0100

    Add LiExplanation component for theme toggling with descriptive images

--- BackgroundToggle/src/components/Explanation/Explanation.jsx
+++ BackgroundToggle/src/components/Explanation/Explanation.jsx
@@ -8,6 +8,18 @@ const Explanation = () => {
         stepNumber={1}
         description={"Init the state with the value of the user's preference"}
         imgUrl={'/images/initHook.png'}
+        imgAlt={
+          'It is a code snippet that shows how to initialize a state with the value of the user preference'
+        }
+      />
+
+      <LiExplanation
+        stepNumber={2}
+        description={'Create a function to toggle the theme'}
+        imgUrl={'/images/toggleTheme.png'}
+        imgAlt={
+          'It is a code snippet that shows how to create a function to toggle the theme'
+        }
       />
     </ol>
   )
