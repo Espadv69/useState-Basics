68293c148f1532000f4676fece7b3e5606502687
Espadv69 <aitorjob69@gmail.com>
2025-03-11 17:19:31 +0100

    Add ThemeToggle component with initial implementation and CSS

---
+++ BackgroundToggle/src/components/ThemeToogle/ThemeToggle.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './ThemeToggle.css'
+
+const ThemeToggle = () => {
+  // Add state to handle the theme
+  const [theme, setTheme] = useState('light')
+
+  // Function to toggle the theme
+}
+
+export default ThemeToggle
