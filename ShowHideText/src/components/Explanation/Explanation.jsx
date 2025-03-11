61723ff9e7a9c25969018c4fde4a846ce6eb8467
Espadv69 <aitorjob69@gmail.com>
2025-03-11 19:32:58 +0100

    Add step 3 to Explanation component for rendering HTML elements with text and button

--- ShowHideText/src/components/Explanation/Explanation.jsx
+++ ShowHideText/src/components/Explanation/Explanation.jsx
@@ -18,6 +18,13 @@ const Explanation = () => {
           imgUrl="/images/toggleText.png"
           imgAlt="Code snippet showing the function to toggle the text visibility."
         />
+
+        <LiExplanation
+          stepNumber={3}
+          description="Rendering HTML elements with the text and button."
+          imgUrl="/images/htmlStructure.png"
+          imgAlt="Code snippet showing the HTML structure with the text and button."
+        />
       </ol>
     </div>
   )
