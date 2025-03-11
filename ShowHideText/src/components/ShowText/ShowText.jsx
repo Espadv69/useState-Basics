d2724e9f02b68a88fac9736f676bebe233eac258
Espadv69 <aitorjob69@gmail.com>
2025-03-11 18:35:15 +0100

    Add toggle functionality to ShowText component

--- ShowHideText/src/components/ShowText/ShowText.jsx
+++ ShowHideText/src/components/ShowText/ShowText.jsx
@@ -2,7 +2,13 @@ import { useState } from 'react'
 import './ShowText.css'
 
 const ShowText = () => {
+  // Initialize the state with false value to hide the text
   const [showText, setShowText] = useState(false)
+
+  // Function to toggle the state value
+  const toggleText = () => {
+    setShowText(!showText)
+  }
 }
 
 export default ShowText
