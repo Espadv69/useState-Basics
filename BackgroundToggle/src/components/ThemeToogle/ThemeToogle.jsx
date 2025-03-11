e80c0d612e9b19c2a0190fa82e8eeef72ee71de8
Espadv69 <aitorjob69@gmail.com>
2025-03-11 17:19:25 +0100

    Remove ThemeToggle component and associated CSS file

--- BackgroundToggle/src/components/ThemeToogle/ThemeToogle.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-import './ThemeToogle.css'
-
-const ThemeToogle = () => {
-  // Add state to handle the theme
-  const [theme, setTheme] = useState('light')
-}
-
-export default ThemeToogle
