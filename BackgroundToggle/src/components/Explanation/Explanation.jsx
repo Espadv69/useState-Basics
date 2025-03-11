7ba93196eed2d2443d0909ccfc2a77b0e2c8efb0
Espadv69 <aitorjob69@gmail.com>
2025-03-11 17:41:36 +0100

    Implement Explanation component to display initial state setup with image

--- BackgroundToggle/src/components/Explanation/Explanation.jsx
+++ BackgroundToggle/src/components/Explanation/Explanation.jsx
@@ -1,5 +1,16 @@
+import LiExplanation from './LiExplanation'
 import './Explanation.css'
 
-const Explanation = () => {}
+const Explanation = () => {
+  return (
+    <ol className="ol__explanation">
+      <LiExplanation
+        stepNumber={1}
+        description={"Init the state with the value of the user's preference"}
+        imgUrl={'/images/initHook.png'}
+      />
+    </ol>
+  )
+}
 
 export default Explanation
