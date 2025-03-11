d11deafbcc944bef3b5f7824d9144438218df5bd
Espadv69 <aitorjob69@gmail.com>
2025-03-11 19:28:26 +0100

    Add rendering logic to Explanation component for displaying initial step

--- ShowHideText/src/components/Explanation/Explanation.jsx
+++ ShowHideText/src/components/Explanation/Explanation.jsx
@@ -1,5 +1,19 @@
+import LiExplanation from './LiExplanation'
 import './Explanation.css'
 
-const Explanation = () => {}
+const Explanation = () => {
+  return (
+    <div className="explanation">
+      <ol className="ol__explanation">
+        <LiExplanation
+          stepNumber={1}
+          description="Initial state with the text hidden."
+          imgUrl="/images/initHook.png"
+          imgAlt="Code snippet showing the initial state with the text hidden."
+        />
+      </ol>
+    </div>
+  )
+}
 
 export default Explanation
