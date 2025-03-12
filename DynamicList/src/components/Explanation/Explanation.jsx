5d60364b92b2fb7bb60d68bf8f43f3e474f689ab
Espadv69 <aitorjob69@gmail.com>
2025-03-12 20:24:01 +0100

    Implement Explanation component to display initial list setup with image

--- DynamicList/src/components/Explanation/Explanation.jsx
+++ DynamicList/src/components/Explanation/Explanation.jsx
@@ -1,6 +1,19 @@
 import LiExplanation from './LiExplanation'
 import './Explanation.css'
 
-const Explanation = () => {}
+const Explanation = () => {
+  return (
+    <section className="explanation">
+      <ol className="ol__explanation">
+        <LiExplanation
+          stepNumber={1}
+          description="Initialize a constant array with the initial list of items."
+          imgUrl="/images/initialList.png"
+          imgAlt="code snippet of an array with 5 items"
+        />
+      </ol>
+    </section>
+  )
+}
 
 export default Explanation
