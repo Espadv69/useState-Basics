822a1d0e6d401a06b3df6ec39ec478ac94c34060
Espadv69 <aitorjob69@gmail.com>
2025-03-12 19:02:29 +0100

    Integrate Input component into App structure

--- RealTimeInput/src/App.jsx
+++ RealTimeInput/src/App.jsx
@@ -1,3 +1,11 @@
-const App = () => {}
+import Input from './components/Input/Input'
+
+const App = () => {
+  return (
+    <div className="app">
+      <Input />
+    </div>
+  )
+}
 
 export default App
