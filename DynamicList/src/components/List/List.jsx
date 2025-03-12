e1ff00a21b23326cd1027044264072bb9bab3403
Espadv69 <aitorjob69@gmail.com>
2025-03-12 19:55:35 +0100

    Add initial list of items to List component in DynamicList

--- DynamicList/src/components/List/List.jsx
+++ DynamicList/src/components/List/List.jsx
@@ -1,5 +1,14 @@
 import './List.css'
 
+// Initial list of items
+const INITIAL_LIST = [
+  'First item',
+  'Second item',
+  'Third item',
+  'Fourth item',
+  'Fifth item',
+]
+
 const List = () => {}
 
 export default List
