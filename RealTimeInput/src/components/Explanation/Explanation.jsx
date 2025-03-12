ccd8bafb55a6e7fa908584beb6511319912fbf1f
Espadv69 <aitorjob69@gmail.com>
2025-03-12 19:15:33 +0100

    Implement Explanation component to render LiExplanation with step details and image

--- RealTimeInput/src/components/Explanation/Explanation.jsx
+++ RealTimeInput/src/components/Explanation/Explanation.jsx
@@ -1,6 +1,19 @@
 import LiExplanation from './LiExplanation'
 import './Explanaation.css'
 
-const Explanation = () => {}
+const Explanation = () => {
+  return (
+    <div className="explanation">
+      <ol className="ol__explanation">
+        <LiExplanation
+          stepNumber={1}
+          description="Initialise the state with an empty string."
+          imgUrl="/images/initHook.png"
+          imgAlt="Code snippet showing the initialisation of the state."
+        />
+      </ol>
+    </div>
+  )
+}
 
 export default Explanation
