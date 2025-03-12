e01049e8b334bfbdeb371d7dbe24f50826f9b31b
Espadv69 <aitorjob69@gmail.com>
2025-03-12 18:55:51 +0100

    Add state management for input value in Input component

--- RealTimeInput/src/components/Input/Input.jsx
+++ RealTimeInput/src/components/Input/Input.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import './Input.css'
 
-const Input = () => {}
+const Input = () => {
+  // State to handle the input value
+  const [inputValue, setInputValue] = useState('')
+}
 
 export default Input
