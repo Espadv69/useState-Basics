9f7045b751b1312447a5b980a1551e4faa198a0d
Espadv69 <aitorjob69@gmail.com>
2025-03-12 19:59:09 +0100

    Initialize input state in List component using useState hook

--- DynamicList/src/components/List/List.jsx
+++ DynamicList/src/components/List/List.jsx
@@ -13,6 +13,8 @@ const INITIAL_LIST = [
 const List = () => {
   // Init the list state with the initial list to have a starting point
   const [list, setList] = useState(INITIAL_LIST)
+  // Init the input state with an empty string
+  const [inputValue, setInputValue] = useState('')
 }
 
 export default List
