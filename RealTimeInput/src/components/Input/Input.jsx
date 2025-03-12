b22f4f3c9cc11b73ad34b7df724ef9d88b0f5908
Espadv69 <aitorjob69@gmail.com>
2025-03-12 18:57:29 +0100

    Add input change handler to Input component

--- RealTimeInput/src/components/Input/Input.jsx
+++ RealTimeInput/src/components/Input/Input.jsx
@@ -4,6 +4,11 @@ import './Input.css'
 const Input = () => {
   // State to handle the input value
   const [inputValue, setInputValue] = useState('')
+
+  // Function to handle the input change
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
+  }
 }
 
 export default Input
